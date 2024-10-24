bebcf85f25f28fc5c8d4452a0a163280a464e0a7
Gabriel-Ricci <gabrielr244@nycstudents.net>
2024-10-24 13:33:19 +0000

    sigma

--- GridPath.java
+++ GridPath.java
@@ -12,7 +12,11 @@ public class GridPath {
      * grid.
      */
     public Location getNextLoc(int row, int col) {
-        /* to be implemented in part (a) */ }
+        int below = grid[row + 1][col];
+        int right = grid[row][col + 1];
+        if(below < right) return new Location(row + 1, col);
+        else return new Location(row, col + 1);
+    }
 
     /**
      * Computes and returns the sum of all values on a path through grid, as
@@ -24,5 +28,23 @@ public class GridPath {
      * grid.
      */
     public int sumPath(int row, int col) {
-        /* to be implemented in part (b) */ }
+        return 0; }
+
+    public GridPath(int[][] values)
+    {
+        grid = values; 
+    }
+    public String toString()
+    {
+        String s = "";
+        for (int row = 0; row < grid.length; row++)
+        {
+            for (int col = 0; col < grid[0].length; col++)
+            {
+                s += grid[row][col] + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
 }
